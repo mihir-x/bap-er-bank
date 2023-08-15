c1807a759338582546f4c795c6cd630412ce7717
mihir-x <ictlancerx@gmail.com>
2023-08-15 13:26:45 +0600

    fixed validation

--- js/deposit.js
+++ js/deposit.js
@@ -2,6 +2,10 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
     const newDepositAmount  = parseFloat(newDepositAmountString);
+    depositField.value = '';
+    if(isNaN(newDepositAmount)){
+        return alert('Please enter the correct ammount');
+    }
     
     const depositTotalElement = document.getElementById('deposit-total');
     const depositTotalString = depositTotalElement.innerText;
@@ -15,6 +19,4 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const balanceTotal = parseFloat(balanceTotalString);
     const newTotalBalance = balanceTotal + newDepositAmount;
     balanceTotalElement.innerText = newTotalBalance;
-
-    depositField.value = '';
 });
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,19 +2,23 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-    
+    withdrawField.value = '';
+    if(isNaN(newWithdrawAmount)){
+        return alert('Please enter the correct ammount');
+    }
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const withdrawTotalString = withdrawTotalElement.innerText;
     const withdrawTotal = parseFloat(withdrawTotalString);
 
-    const newWithdrawTotal = withdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = newWithdrawTotal;
-
     const balanceTotalElement = document.getElementById('balance-total');
     const balanceTotalString = balanceTotalElement.innerText;
     const balanceTotal = parseFloat(balanceTotalString);
+    if(balanceTotal<newWithdrawAmount){
+        
+        return alert('ohho not gonna happen');
+    }
+    const newWithdrawTotal = withdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = newWithdrawTotal;
     const newTotalBalance = balanceTotal -newWithdrawAmount;
     balanceTotalElement.innerText = newTotalBalance;
-
-    withdrawField.value = '';
 })
